8c8bd5fc2bc3960d8dab0b1ba579dac8d1c2c69a
Mohsen HADAVI <mohsen.hadavi@etu.emse.fr>
2024-01-04 20:54:29 +0100

    add credentials

--- src/emse/aws/ShopInventory.java
+++ src/emse/aws/ShopInventory.java
@@ -4,8 +4,8 @@ import software.amazon.awssdk.regions.Region;
 
 public class ShopInventory {
     public static Region region = Region.US_EAST_1;
-    public static String bucket = "" ;
-    public static String topicARN = "" ;
-    public static String sqsURL = "" ;
+    public static String bucket = "cloudtp0" ;
+    public static String topicARN = "arn:aws:sns:us-east-1:981161034142:tpSNS" ;
+    public static String sqsURL = "https://sqs.us-east-1.amazonaws.com/981161034142/tpQueue" ;
     public static String salesData = "src/emse/aws/sales_data" ;
 }
